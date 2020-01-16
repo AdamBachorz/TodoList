20c9a7940bf2ca55de12e705bcbc76c1ff1d0987
AdamBachorz <adam.bachorz1702@gmail.com>
2020-01-16 15:14:40 +0100

    Add DateTime extension method

---
+++ Model.Test/Extensions.Test/DateTimeExtensionsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Model.Extensions;
+using NUnit.Framework;
+
+namespace Model.Test.Extensions.Test
+{
+    [TestFixture]
+    public class DateTimeExtensionsTest
+    {
+        [TestCase(2020, 1, 2, ExpectedResult = 1)]
+        [TestCase(2020, 1, 8, ExpectedResult = 2)]
+        [TestCase(2020, 12, 22, ExpectedResult = 52)]
+        public int GetWeekOfYear_ShouldReturnCorrectWeekOfYear(int year, int month, int day)
+        {
+            return new DateTime(year, month, day).GetWeekOfYear();
+        }
+    }
+}
---
+++ Model/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int GetWeekOfYear(this DateTime time)
+        {
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                time = time.AddDays(3);
+            }
+
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+
+    }
+}
