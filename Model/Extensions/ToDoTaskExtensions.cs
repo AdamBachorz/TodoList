af29916d0100b606fcde2eb83146d7296b918fa4
AdamBachorz <adam.bachorz1702@gmail.com>
2020-01-16 15:37:59 +0100

    Add ToDoTaskExtension

---
+++ Model/Extensions/ToDoTaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Model.DataAccess.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Extensions
+{
+    public static class ToDoTaskExtensions
+    {
+        public static bool HasValidReminder(this ToDoTask toDoTask)
+        {
+            var taskDate = toDoTask.ToDoList.Date;
+            var currentDate = DateTime.Now.Date;
+            var taskNumberOfWeek = taskDate.GetWeekOfYear();
+            var currentNumberOfWeek = currentDate.GetWeekOfYear();
+
+            return toDoTask.ToRemind && taskNumberOfWeek == currentNumberOfWeek 
+                && DateTime.Compare(currentDate, taskDate) >= 0;
+        }
+    }
+}
